8d9436e89e0038fb5ee551e9d8d68ddb1082352e
ShivaBansfore-JGEC <shivabansfore123@gmail.com>
2023-09-15 08:49:04 +0530

    feat: initial structure setup

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+const {PORT} = require('./config/serverConfig');
+const apiRoutes = require('./routes/index');
+const db = require('./models/index');
+
+const setupAndStartServer = async () => {
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended: true}))
+    app.use('/api', apiRoutes);
+    app.listen(PORT, () => {
+        console.log(`server started on port: ${PORT}`);
+        if(process.env.DB_SYNC){
+            db.sequelize.sync({alter: true})
+        }
+    })
+}
+
+setupAndStartServer();
