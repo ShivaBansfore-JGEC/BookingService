01634b7f2b589892837b882d266da8eaf5f1f818
ShivaBansfore-JGEC <shivabansfore123@gmail.com>
2023-09-15 21:45:50 +0530

    feat: added error files

---
+++ src/utils/errors/app-error.js
@@ -0,0 +1,12 @@
+
+
+class AppError extends Error {
+    constructor(name, message, explaination, statusCode){
+        this.name = name;
+        this.message = message;
+        this.explaination = explaination;
+        this.statusCode = statusCode;
+    }
+}
+
+module.exports = AppError;
